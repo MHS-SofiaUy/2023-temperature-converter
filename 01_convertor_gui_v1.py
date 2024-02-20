74a1cbb987843fc5fb29b567a1ba322c4d787e6a
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2024-02-21 12:42:03 +1300

    Checks valid temperature input

--- 01_convertor_gui_v1.py
+++ 01_convertor_gui_v1.py
@@ -5,6 +5,13 @@ class Converter:
 
     def __init__(self):
 
+        # Initialise variables (such as the feedback variable)
+        self.var_feedback = StringVar()
+        self.var_feedback.set("")
+
+        self.var_has_error = StringVar()
+        self.var_has_error.set("no")
+
         # common format for all buttons
         # Arial size 14 bold, with white text
         button_font = ("Arial", "14", "bold")
@@ -35,7 +42,7 @@ class Converter:
         self.temp_entry.grid(row=2, padx=10, pady=10)
 
         error = "Please enter a number"
-        self.temp_error = Label(self.temp_frame, text=error,
+        self.temp_error = Label(self.temp_frame, text="",
                                 fg="#9C0000")
         self.temp_error.grid(row=3)
 
@@ -44,22 +51,25 @@ class Converter:
         self.button_frame.grid(row=4)
 
         self.to_celsius_button = Button(self.button_frame,
-                                        text="To Degrees C",
+                                        text="To Celsius",
                                         bg="#990099",
-                                        fg="#FFFFFF")
-        self.to_celsius_button.grid(row=0, column=0)
+                                        fg="#FFFFFF",
+                                        font=button_font, width=12,
+                                        command=self.to_celsius)
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
 
         self.to_fahrenheit_button = Button(self.button_frame,
                                            text="To Fahrenheit",
                                            bg="#009900",
-                                           fg="#FFFFFF")
+                                           fg="#FFFFFF",
+                                           font=button_font, width=12)
         self.to_fahrenheit_button.grid(row=0, column=1)
 
         self.to_help_button = Button(self.button_frame,
-                                        text="Help / Info",
-                                        bg="#CC6600",
-                                        fg=button_fg,
-                                        font=button_font, width=12)
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     fg=button_fg,
+                                     font=button_font, width=12)
         self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
 
         self.to_history_button = Button(self.button_frame,
@@ -70,23 +80,41 @@ class Converter:
                                         state=DISABLED)
         self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
 
+    # checks user input and if it's valid, converts temperature
+    def check_temp(self, min_value):
 
-def check_temp(min_value):
-    error = "Please enter a number that is more " \
-            "than {}".format(min_value)
+        has_error = "no"
+        error = "Please enter a number that is more " \
+                "than {}".format(min_value)
 
-    try:
-        response = float(input("Chose a number: "))
+        # check that user has entered a valid number...
+        try:
+            response = self.temp_entry.get()
+            response = float(response)
 
-        if response < min_value:
-            print(error)
+            if response < min_value:
+                has_error = "yes"
+
+        except ValueError:
+            has_error = "yes"
+
+        # If the number is invalid, display error message
+        if has_error == "yes":
+            self.temp_error.config(text=error, fg="#9C0000")
         else:
-            return response
+            self.temp_error.config(text="You are OK", fg="blue")
 
-    except ValueError:
-        print(error)
+            # if we have at least one valid calculation,
+            # enable history / export button
+            self.to_history_button.config(state=NORMAL)
+
+    def to_celsius(self):
+        self.check_temp(-459)
+
 
 # main routine
+
+
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Converter")
